30b78d655c724d7cd8efea4eab2b0c5c7839b69c
Thibaud Colas <thibaudcolas@gmail.com>
2013-03-01 03:38:18 +0100

    Add header w/ ticker to explorer

--- datacubeviz/src/web/public/static/js/viewmodels/explorer.js
+++ datacubeviz/src/web/public/static/js/viewmodels/explorer.js
@@ -20,11 +20,22 @@ define([
     self.initialize = function () {
       $.getJSON(g.remoteURL + g.paths.datasets + '?project=' + self.project(),
         function (data) {
-          // Our main working entity is a dataset.
+          // Our main working entity is a dataset, we add them to our view model.
           $.each(data, function (i, source) {
             var qbSource = new DataCubeSource(source.title || "SRC Ø", source.uri, self.project());
             $.each(source.datasets, function (j, dataset) {
               self.datasets.push(new DataCubeDataset(dataset.title || "DS Ø", dataset.uri, qbSource));
+              self.datasets.push(new DataCubeDataset(dataset.title || "DS2 Ø", dataset.uri, qbSource));
+              self.datasets.push(new DataCubeDataset(dataset.title || "DS3 Ø", dataset.uri, qbSource));
+            });
+          });
+
+          // This is only to test.
+          $.each(data, function (i, source) {
+            var qbSource = new DataCubeSource(source.title || "SRC2 Ø", source.uri, self.project());
+            $.each(source.datasets, function (j, dataset) {
+              self.datasets.push(new DataCubeDataset(dataset.title || "DS4 Ø", dataset.uri, qbSource));
+              self.datasets.push(new DataCubeDataset(dataset.title || "DS5 Ø", dataset.uri, qbSource));
             });
           });
 
