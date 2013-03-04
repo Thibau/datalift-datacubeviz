dc65a49a6a293a5c03b225de1396072901005094
Thibaud Colas <thibaudcolas@gmail.com>
2013-03-04 08:55:37 +0100

    Now w/ example NVD3 binding

--- datacubeviz/src/web/public/static/js/extends/handlers.js
+++ datacubeviz/src/web/public/static/js/extends/handlers.js
@@ -3,7 +3,8 @@ define([
   'knockout',
   'config/global',
   'bootstrap',
-  'datatables'
+  'datatables',
+  'nvd3'
 ], function ($, ko, g) {
   'use strict';
 
@@ -105,4 +106,60 @@ define([
         }
       }
     };
+
+
+    ko.bindingHandlers.nvChart = {
+      init: function (element, valueAccessor) {
+
+      },
+      update: function (element, valueAccessor) {
+        var binding = ko.utils.unwrapObservable(valueAccessor());
+
+        if (binding) {
+          var data = function() {
+            var sin = [],
+                cos = [];
+
+            for (var i = 0; i < 100; i++) {
+              sin.push({x: i, y: Math.sin(i/10)});
+              cos.push({x: i, y: .5 * Math.cos(i/10)});
+            }
+
+            return [
+              {
+                values: sin,
+                key: 'Sine Wave',
+                color: '#ff7f0e'
+              },
+              {
+                values: cos,
+                key: 'Cosine Wave',
+                color: '#2ca02c'
+              }
+            ];
+          };
+
+          nv.addGraph(function() {
+            var chart = nv.models.lineChart();
+
+            chart.xAxis
+                .axisLabel('Time (ms)')
+                .tickFormat(d3.format(',r'));
+
+            chart.yAxis
+                .axisLabel('Voltage (v)')
+                .tickFormat(d3.format('.02f'));
+
+            d3.select('#chart svg')
+                .datum(data())
+              .transition().duration(500)
+                .call(chart);
+
+            nv.utils.windowResize(chart.update);
+
+            return chart;
+          });
+        }
+      }
+    }
 });
