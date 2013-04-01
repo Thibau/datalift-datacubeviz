97bc3a632ab04c085caa3c75c7dbe0218891ad67
Thibaud Colas <thibaudcolas@gmail.com>
2013-04-01 21:39:21 +0200

    Add comments

--- datacubeviz/src/web/public/static/js/models/datacube/Observation.js
+++ datacubeviz/src/web/public/static/js/models/datacube/Observation.js
@@ -3,6 +3,12 @@ define([
 ], function (g) {
   'use strict';
 
+  /**
+   * An observation is a DataCube entity which is a part of a statistical dataset.
+   * @param {String} identifier The unique identifier for the observation (BNode, URI).
+   * @param {Object} components The components of the observation : key/value pairs,
+   * where key is the predicate and value its object.
+   */
   var Observation = function (identifier, components) {
     var self = this;
 
