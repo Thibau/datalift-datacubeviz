281936b527f291e6142f869084b8e48de983ee56
Thibaud Colas <thibaudcolas@gmail.com>
2013-04-01 20:39:14 +0200

    Commenting to make things clearer

--- datacubeviz/src/web/public/static/js/config/global.js
+++ datacubeviz/src/web/public/static/js/config/global.js
@@ -1,10 +1,12 @@
 define({
   ENTER_KEY: 13,
+  // URL principale du module (i.e http://localhost:8080/datalift/datacubeviz).
   remoteURL : window.location.href.substring(0, window.location.href.indexOf('?')),
   datasetsPerRow : 3,
   project : window.location.href.substring(window.location.href.indexOf('?'), window.location.href.length).replace('?project=', ''),
   paths : {
     datasets : '/ws/datasets',
+    // This should be temporary (ahem...), gets the URL to the Datalift SPARQL endpoint.
     endpoint : window.location.href.substring(0, window.location.href.indexOf('?')).replace('datacubeviz', 'sparql')
   },
   tabs : {
